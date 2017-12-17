34ee4393318ef5eb9e93b4567905a5218ebc197a
Jimin Kang <jimin.kang91@gmail.com>
2017-12-17 19:10:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,8 +19,11 @@ function sayHitoGrandma (string){
     return "YES INDEED!"
   }
   if(string.toLowerCase() == string){
-    return
+    return "I can\'t hear you!"
   }
+  if(string == "I love you, Grandma/"){
+    return "I love you, too."
   }
+  
   
 }
